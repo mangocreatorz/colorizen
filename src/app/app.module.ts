150bc2e70ae3356252e8cb22740705e4eba00f82
Sergey <mangocreatorz@gmail.com>
2021-05-15 13:00:24 +0600

    removed syncfusion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {UploaderAllModule} from '@syncfusion/ej2-angular-inputs';
-import {CheckBoxAllModule} from '@syncfusion/ej2-angular-buttons';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatDialogModule} from '@angular/material/dialog';
 import {UploadFormComponent} from './upload-form/upload-form.component';
@@ -25,8 +23,6 @@ import {UploadFormComponent} from './upload-form/upload-form.component';
     HttpClientModule,
     ReactiveFormsModule,
     FormsModule,
-    UploaderAllModule,
-    CheckBoxAllModule,
     BrowserAnimationsModule,
     MatDialogModule
 
